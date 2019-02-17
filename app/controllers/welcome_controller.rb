aec7492b8540084187cbf0002e2067fb100f4b12
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-02-17 13:50:54 -0300

    Create Welcome Controller and Views

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class WelcomeController < ApplicationController
+  layout 'welcome'
+  
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_scope :users do
+  root to: 'welcome#index'
+
+  ## Devise Views
+  devise_scope :user do
     # custom path to login/sign_in to sign_up/registration
     get '/sign_in', to: 'devise/sessions#new'
     get '/sign_up', to: 'devise/registrations#new', as: 'new_user_registration'
