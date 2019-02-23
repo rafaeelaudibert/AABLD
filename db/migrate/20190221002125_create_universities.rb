8fb4983031c64cbd8d013633b39396d36bf307de
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-02-23 01:40:04 -0300

    :recycle: Update University model

--- db/migrate/20190221002125_create_universities.rb
+++ db/migrate/20190221002125_create_universities.rb
@@ -1,11 +1,12 @@
 class CreateUniversities < ActiveRecord::Migration[5.2]
   def change
     create_table :universities do |t|
-      t.string :name
-      t.string :cnpj, index: true
+      t.string :name, null: false
+      t.string :abbreviation, index: true, null: false
+      t.string :cnpj, index: true, null: false
       t.references :city, foreign_key: true
-      t.string :neighborhood
-      t.string :address
+      t.string :neighborhood, default: 'Não se aplica'
+      t.string :address, default: 'Não se aplica'
     end
   end
 end
