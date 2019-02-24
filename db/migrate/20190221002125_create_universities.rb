9a32c5bf9418d9ab764b670e4ad9f294e8c6e9b9
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-02-24 01:09:34 -0300

    :recycle: Add CNPJ size limit constraint to the University db table

--- db/migrate/20190221002125_create_universities.rb
+++ db/migrate/20190221002125_create_universities.rb
@@ -3,7 +3,7 @@ class CreateUniversities < ActiveRecord::Migration[5.2]
     create_table :universities do |t|
       t.string :name, null: false
       t.string :abbreviation, index: true, null: false
-      t.string :cnpj, index: true, null: false
+      t.string :cnpj, index: true, null: false, limit: 18
       t.references :city, foreign_key: true
       t.string :neighborhood, default: 'Não se aplica'
       t.string :address, default: 'Não se aplica'
