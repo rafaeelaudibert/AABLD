7d9a46475a7067a9075dddd40dbdff17b4d2e797
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-03-17 22:08:01 -0300

    :recycle: Add custom Devise mailer translations

--- app/mailers/aabld_devise_mailer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AabldDeviseMailer < Devise::Mailer
-  default from: 'associacaoAABLD@gmail.com'
-
-  def self.mailer_name
-    'devise/mailer'
-  end
-end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,16 +67,16 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'piubs.inf.ufrgs.br',
+    domain: Rails.application.secrets.domain_name,
     authentication: 'plain',
     enable_starttls_auto: true,
-    user_name: 'associacaoAABLD@gmail.com',
-    password: 'AABLD2019'
+    user_name: 'apoio.piubs@gmail.com',
+    password: 'piubs@ufrgs123'
   }
 
   # ActionMailer Config
   config.active_job.queue_adapter = :sidekiq
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost:8081' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
 end
