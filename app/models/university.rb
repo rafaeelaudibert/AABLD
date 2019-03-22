103948f13c28d47570ef4c44480e2d2dd66b78ca
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-03-22 11:03:05 -0300

    :sparkles::lipstick: Update University index page

--- app/models/university.rb
+++ app/models/university.rb
@@ -2,13 +2,19 @@
 
 class University < ApplicationRecord
   belongs_to :city
+  has_many :users
 
   validates :name, presence: true
   validates_cnpj_format_of :cnpj, options: { allow_blank: true, allow_nil: true }
 
   delegate :state, to: :city
+  delegate :state_abbreviation, to: :city
 
-  def name_and_abbr
+  def abbr_and_name
     "#{abbreviation} | #{name}"
+  end
+
+  def city_and_state
+    "#{city.name}/#{city.state_abbreviation}"
   end
 end
