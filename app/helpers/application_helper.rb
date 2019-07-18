83b8260289d6e5546f99baef11a466543fb004b0
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-07-18 11:05:21 -0300

    :sparkles: Added helpers for apex charts creation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,19 @@ module ApplicationHelper
   def paginate(pagy)
     "<div class='float-right'>#{pagy_bootstrap_nav(pagy)}</div>"
   end
+
+  def date_hash_string(date)
+    "#{date.month}/#{date.year}"
+  end
+
+  def create_chart_options(title: 'Title', subtitle: 'Subtitle', xtitle: 'Xtitle', ytitle: 'Ytitle', palette: 'palette4')
+    {
+      title: title,
+      subtitle: subtitle,
+      xtitle: xtitle,
+      ytitle: ytitle,
+      theme: palette,
+      xaxis: { labels: { format: 'MMM' } }
+    }
+  end
 end
