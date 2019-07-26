f2b2d2df2c3c4bd2545c6dbb985b15280d26a711
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-07-26 18:05:30 -0300

    :sparkes: Added new edit features for the User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,20 @@ class UsersController < ApplicationController
 
   def edit; end
 
+  # POST /user/:id/edit
   def update
+    if current_user == @user # Need to use the password to update their data
+      update_with_password
+    else # Is admin or on direction, and doesn't need password
+      update_without_password
+    end
+  end
+
+  private
+
+  def update_without_password
     respond_to do |format|
-      if @user.update(user_params)
+      if @user.update(admin_user_params)
         format.html { redirect_to @user, notice: 'Usuário atualizado com sucesso.' }
         format.json { render :show, status: :ok, location: @user }
       else
@@ -30,10 +41,30 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+  def admin_user_params
+    params.require(:user).permit(:first_name, :last_name, :cpf, :rg, :birthdate,
+                                 :phone, :mobile_phone, :address, :mother_name, :father_name,
+                                 :bank_account, :bank_agency, :bank_option,
+                                 :university_id, :place, :course, :responsible_id, :semester,
+                                 :ticket_responsible)
+  end
 
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :cpf, :rg)
+  def update_with_password
+    respond_to do |format|
+      if @user.update_with_password(with_password_user_params)
+        format.html { redirect_to @user, notice: 'Usuário atualizado com sucesso.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def with_password_user_params
+    params.require(:user).permit(:first_name, :last_name, :cpf, :rg, :birthdate,
+                                 :phone, :mobile_phone, :address, :mother_name, :father_name,
+                                 :about, :current_password)
   end
 
   def current_ability
