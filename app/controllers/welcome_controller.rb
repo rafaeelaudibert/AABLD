7bd790f658baad5c5ada7293f79977f5d5ff1a3e
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-08-02 03:39:47 -0300

    :sparkles: Update welcome screen

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,12 @@ class WelcomeController < ApplicationController
   layout false
   skip_before_action :authenticate_user!
 
-  def index; end
+  def index
+    @has_direction = User.on_direction.count > 0 && false
+    if @has_direction
+      @president = User.president.limit(1).first
+      @treasurer = User.treasurer.limit(1).first
+      @secretary = User.secretary.limit(1).first
+    end
+  end
 end
