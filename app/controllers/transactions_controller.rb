d84db55546ed845b15935cf9fc369ed26f8539a4
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-09-23 22:50:56 -0300

    :recycle: Fetch next transaction ordered by user place

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -96,7 +96,7 @@ class TransactionsController < ApplicationController
     {
       month: Transaction.current_month_index,
       year: Transaction.current_year,
-      user_id: User.not_did_monthly_transaction.first.id
+      user_id: User.order(:place).not_did_monthly_transaction.first.id
     }
   end
 
